73dd4e4c9ceee68e3aaafc710e076b6fa661c91d
asmae <echaib.asmae@gmail.com>
2022-01-10 13:55:42 +0000

    DTO

---
+++ src/main/java/ma/ensa/Transfert/TransfertDTO.java
@@ -0,0 +1,12 @@
+package ma.ensa.Transfert;
+
+public class TransfertDTO {
+    private Long id;
+    private double montant;
+    private Long ref;
+    private double commission;
+    private String motif;
+    private Long agentId;
+    private Long clientId;
+    private Long beneficiareId;
+}
